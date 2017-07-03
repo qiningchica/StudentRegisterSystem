14d118632d101d06459a5a6a9ad03f17f09eec16
09143797 <1711621532@qq.com>
2017-07-03 23:48:09 +0800

    .

---
+++ StudentRegisterSystem/src/mock/ScheduleDaoImpl.java
@@ -0,0 +1,17 @@
+//package mock;
+//
+//import dao.ScheduleDao;
+//import model.ScheduleOfClasses;
+//
+//public class ScheduleDaoImpl implements ScheduleDao {
+//
+//	@Override
+//	public ScheduleOfClasses getScheduleOfClass(String semaster) {
+//		// TODO Auto-generated method stub
+//		
+//		
+//		return null;
+//	}
+//	
+//
+//}
---
+++ StudentRegisterSystem/src/mock/UserDaoImpl.java
@@ -0,0 +1,7 @@
+//package mock;
+//
+//import dao.UserDao;
+//
+//public class UserDaoImpl implements UserDao{
+//
+//}
